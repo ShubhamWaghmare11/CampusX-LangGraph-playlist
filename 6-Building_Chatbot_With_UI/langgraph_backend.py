58329b301b7642f6465707c13ab2d4bd95326b74
maskgamer <gamermask64@gmail.com>
2025-08-08 20:53:19 +0530

    Added message streaming

--- 6-Building_Chatbot_With_UI/langgraph_backend.py
+++ 6-Building_Chatbot_With_UI/langgraph_backend.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_groq import ChatGroq
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
@@ -26,3 +26,4 @@ graph.add_edge("chat_node",END)
 checkpointer = InMemorySaver()
 
 chatbot = graph.compile(checkpointer=checkpointer)
+   
